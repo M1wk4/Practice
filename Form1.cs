0566255a5d69aed4a5a212f1be1e2c470acd7624
Николай Лазаренко <nikolay.laz@outlook.com>
2021-07-05 15:32:43 +0300

    Начало авторизации

--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using VkNet;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+using System.IO;
 
 namespace WindowsFormsApp2
 {
@@ -23,9 +26,26 @@ namespace WindowsFormsApp2
            
         }
 
+        public static string getAuthForGroup()
+        {
+            string fileName = @"auth_vk.txt";
+            string token = "10d744f6cdb1d5d9d43394930b77532b39319202db531c07b9c0e2383c266fe43aadaf04945bb742b8398&expires_in=86400&user_id=253662157";
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    token = sr.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return token;
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            label1.Text = getAuthForGroup();
         }
     }
 }
