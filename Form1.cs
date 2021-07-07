18f2feb6999985e637f40bf77d319ab7fe113cbf
D X <DMXikka@gmail.com>
2021-07-07 14:44:34 +0300

    РЕЛИЗ

--- Form1.cs
+++ Form1.cs
@@ -117,9 +117,9 @@ namespace WindowsFormsApp2
 			textBox3.Text += $"ID: {p.Id.ToString()}\r\n";
 			textBox3.Text += $"Имя: {Encoding.UTF8.GetString(Encoding.Default.GetBytes(p.FirstName))}\r\n";
 			textBox3.Text += $"Фамилия: {Encoding.UTF8.GetString(Encoding.Default.GetBytes(p.LastName))}\r\n";
-			if (Convert.ToInt32(p.Sex) == 1)
+			if (Convert.ToInt32(p.Sex) == 2)
 				textBox3.Text += $"Пол: мужской\r\n";
-			else if (Convert.ToInt32(p.Sex) == 0)
+			else if (Convert.ToInt32(p.Sex) == 1)
 				textBox3.Text += $"Пол: женский\r\n";
 			else
 				textBox3.Text += $"Пол: не указан\r\n";
